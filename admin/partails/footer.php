716f235657c37eb4a630a96cf3a45448d3fd357e
dassomen <somend265@gmail.com>
2024-10-20 09:20:12 +0530

    footer a tag color change

--- admin/partails/footer.php
+++ admin/partails/footer.php
@@ -1,7 +1,7 @@
  <!-- footer section start -->
  <div class="footer">
         <div class="wrapper">
-                <p class="text-center">2024 All Rights Reserved, some restaurent Developed by <a href="#">Somen Das</a> </p>
+                <p class="text-center">2024 All Rights Reserved, some restaurent Developed by  <a href="#"> Somen Das</a> </p>
                 </div>
             </div>
         <!-- footer section end -->
